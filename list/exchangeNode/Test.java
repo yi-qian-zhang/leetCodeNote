b3e2a09659fe0fc06fd3c97178c879c1b3f3fef2
yi-qian-zhang <15065170282@163.com>
2023-10-26 15:17:38 +0800

    2023.10.26-15:17

--- list/exchangeNode/Test.java
+++ list/exchangeNode/Test.java
@@ -12,32 +12,32 @@ public class Test {
         ListNode head2 = new ListNode(3);
         ListNode head3 = new ListNode(4);
         ListNode head4 = new ListNode(5);
-
         head.next=head1;
         head1.next=head2;
         head2.next=head3;
         head3.next=head4;
+        System.out.println(head);
+        System.out.println("--------------------");
         System.out.println(swapPairs(head));
-        System.out.println(swapPairs(null));
     }
 
     public static ListNode swapPairs(ListNode head) {
-        if (head==null){
-            return head;
-        }else {
-            ListNode pre =head;
-            ListNode cur =head.next;
-            ListNode dir =cur.next;
-            while (cur!=null){
+        ListNode dum = new ListNode(0,head);
+        ListNode cur =dum;
+        ListNode start =null;
+        ListNode end =null;
+        ListNode temp3 =null;
+            while (cur.next!=null&&cur.next.next!=null){
                 //翻转
-                cur.next=pre;
-                pre.next=dir;
-                //交换索引
-                ListNode temp = pre;
-                pre = cur;
-                cur = temp;
+                start = cur.next;//1
+                end = cur.next.next;//dum指向2
+                temp3 = cur.next.next.next;//3
+                cur.next = start.next;
+                start.next = temp3;
+                end.next=start;
+                //更新cur
+                cur = start;
             }
-            return head.next;
-        }
+            return dum.next;
     }
 }
